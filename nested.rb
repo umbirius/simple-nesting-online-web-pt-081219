dab3db463a7c745bd622fed46dcb39ab03fbe977
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-19 16:18:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,8 +21,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-programmer_hash[:alan_kay][:known_for]
-	
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -38,6 +37,7 @@ programmer_hash[:alan_kay][:known_for]
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
